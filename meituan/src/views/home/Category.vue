14013792a252120d98c78ead8d7aa7064af3b923
liuxueying123 <1390528287@qq.com>
2020-11-10 23:02:56 +0800

    11.10

---
+++ meituan/src/views/home/Category.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <ul class="list-box">
+            <li class="list" v-for="obj in list" :key="obj.id" >
+                <!-- 动态绑定 -->
+                <img :src="obj.img" alt=""> 
+                <p>{{obj.name}}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+    export default {
+        data(){
+            return {
+                list:[]
+            }
+        },
+        created(){
+            axios.get("http://admin.gxxmglzx.com/tender/test/get_type")
+            .then((res)=>{//成功
+                //axios 返回的数据 res.data
+                // console.log(res.data.data)
+                if(res.data.errcode == 200){
+                    this.list = res.data.data;
+                }
+            }).catch(()=>{//失败
+
+            })
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.list-box{
+    display: flex;
+    flex-wrap: wrap;
+}
+.list{
+    width: 20%;
+    text-align: center;
+    margin-top: 0.3rem;
+}
+.list img{
+    width: 60%;
+}
+</style>
---
+++ meituan/src/views/home/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <!-- Head组件-->
+        <Head></Head>
+        <!-- Category组件 -->
+        <Category></Category>
+        <!-- List组件 -->
+        <List></List>
+    </div>
+</template>
+
+<script>
+
+ /*1.impor引入
+   2.compoments声明
+   3.template使用
+ */
+    import Head from './Head'
+    import Category from './Category'
+    import List from './List'
+    export default {
+        components:{
+            Head,
+            Category,
+            List
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
